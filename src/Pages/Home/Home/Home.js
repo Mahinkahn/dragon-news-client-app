9d686df1f42ae41279ecbdd17ba0c7c23a18f174
Mahinkahn <mahinkahn37@gmail.com>
2022-11-19 20:23:51 +0600

    http://localhost:5000/news

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Home = () => {
+    const allnews = useLoaderData();
     return (
         <div>
-            <h2>Home</h2>
+            <h2>Dragon News Home : {allnews.length}</h2>
+            {
+                allnews.map(news => <NewsSummaryCard key={news._id}
+                    news={news}></NewsSummaryCard>)
+            }
         </div>
     );
 };
